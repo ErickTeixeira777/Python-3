9d19eb40e6b5b08595ec037f7adf5530ccec985c
Erick Teixeira <contatoerickvenon@gmail.com>
2022-01-10 15:23:07 -0300

    up date

--- Ex16.py
+++ Ex16.py
@@ -1,1 +1,8 @@
-# Utilizando Módulos
+# Quebrando um número
+# Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção inteira! ex: Digite um número: 6.127 O número 6.127 tem a parte Inteira 6.
+
+# import math
+from math import trunc
+num = float(input('Digite um valor: '))
+# print('O valor digitado foi {} e sua porção inteira é {}' .format(num, math.trunc(num)))
+print('O valor digitado foi {} e sua porção inteira é {}' .format(num, trunc(num)))
