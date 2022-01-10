50a464eb59646f3f15c1a283554afd970bef051e
Erick Teixeira <contatoerickvenon@gmail.com>
2022-01-10 00:20:01 -0300

    up date

---
+++ Ex12.py
@@ -0,0 +1,5 @@
+# Calculando descontos
+# Faça um algoritmo que leia o preço de um produto e mostra seu novo preço, com 5% de desconto.
+preço = float(input('Qual é o preço do produto? R$'))
+novo = preço -  (preço * 5/ 100)
+print('O produto que custava R${:.2f} \nNa promoção com desconto de 5% vai custar R${:.2f}' .format(preço, novo))
