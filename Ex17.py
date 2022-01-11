4b81b5afef3ec826bda59036995a4c6d49af71c8
Erick Teixeira <contatoerickvenon@gmail.com>
2022-01-10 21:14:19 -0300

    up date

---
+++ Ex17.py
@@ -0,0 +1,12 @@
+'''Catetos e Ho
+ipotenusa!
+Faça um programa que leia ocomprimento do cateto oposto e do cateto adjacente de um triangulo retângulo, calcule e mostre o comprimento da hipotenusa'''
+'''co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+hi = (co ** 2 + ca ** 2) ** (1/2)
+print('A hipotenusa vai medir {:.2f}' .format(hi))'''
+import math
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+hi = math.hypot(co, ca)
+print('A hipotenusa vai medir {:.2f}' .format(hi))
