77cf09222c4247cb77184e166c5572f7b04ad70e
Erick Teixeira <contatoerickvenon@gmail.com>
2022-01-11 13:52:12 -0300

    up date

--- Ex21.py
+++ Ex21.py
@@ -1,5 +1,5 @@
-'''Faça um programa em Python que abra e reproduza o aúdio de um arquivo MP3.'''
 import pygame
+
 pygame.init()
 pygame.mixer.music.load('happy-mistake.mp3')
 pygame.mixer.music.play()
