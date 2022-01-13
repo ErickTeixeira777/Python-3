df6e7a81ff4e13dfd9c44611baac9b8073408ce2
Erick Teixeira <contatoerickvenon@gmail.com>
2022-01-13 01:28:26 -0300

    up date

--- Ex26.py
+++ Ex26.py
@@ -1,2 +1,7 @@
-# Primeira e última ocorrência de uma string
-frase = str(input('Digite uma frase: '))
+# Primeira e última ocorrência de uma string 
+# Faça um programa que leia uma frase pelo teclado e mostre: 
+# Em que posição ela aparece a primeira e última vez.
+frase = str(input('Digite uma frase: ')).upper().strip()
+print('Aletra A aparece {} vezes na frase. '.format(frase.count('A')))
+print('A primeira letra A apareceu na posição {}'.format(frase.find('A')+1))
+print('A útima letra A apareceu na posição {}'.format(frase.rfind('A')+1))
