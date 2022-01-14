748059c21cb482fe3dd3b565f79b9daa1239eb00
Erick Teixeira <contatoerickvenon@gmail.com>
2022-01-14 19:20:55 -0300

    up date

---
+++ aula11.py
@@ -0,0 +1,6 @@
+#CORES NO TERMINAL
+nome = 'Erick Teixeira'
+cores = {'limpa':'\033[m',        'azul':'\033[34m','amarelo':'\033[33m','pretoebranco':'\033[7;30m'}
+print('!!{}{}{}!!'.format(cores['azul'], nome, cores['limpa']))
+
+
