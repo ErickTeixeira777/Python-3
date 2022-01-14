ada904ac302c5865a494fc2495272a12a2d9c813
Erick Teixeira <contatoerickvenon@gmail.com>
2022-01-14 11:44:55 -0300

    up date

--- Ex26.py
+++ Ex26.py
@@ -2,6 +2,6 @@
 # Faça um programa que leia uma frase pelo teclado e mostre: 
 # Em que posição ela aparece a primeira e última vez.
 frase = str(input('Digite uma frase: ')).upper().strip()
-print('Aletra A aparece {} vezes na frase. '.format(frase.count('A')))
+print('A letra A aparece {} vezes na frase. '.format(frase.count('A')))
 print('A primeira letra A apareceu na posição {}'.format(frase.find('A')+1))
 print('A útima letra A apareceu na posição {}'.format(frase.rfind('A')+1))
---
+++ Ex34.py
@@ -0,0 +1,10 @@
+'''Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento.
+Para salário superiores a R$1.200,00\nCalcule um aumento de 10%.
+Para os inferiores ou iguais, o aumento é de 15%. '''
+
+salário = float(input('Qual é o salário do funcionário?'))
+if salário <= 1250:
+    novo = salário + (salário * 15/100)
+else:
+    novo = salário + (salário * 10/100)
+print('Quem ganhava R${:.2f} passa a ganhar R${:.2f} agora.'.format(salário, novo))        
