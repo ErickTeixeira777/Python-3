e9cb53708342a571678bb28a44d35aa4fcf9e33f
Erick Teixeira <contatoerickvenon@gmail.com>
2022-01-13 21:46:46 -0300

    up date

--- Ex31.py
+++ Ex31.py
@@ -5,5 +5,5 @@ print('Você está prestes a começar uma viagem de {}Km.'.format(distância))
     #preço = distância * 0.50
 #else:
     #preço = distância * 0.45   
-preço = distância * 0.50 if distância <=200 else distância * 0.45    
+preço = distância * 0.50 if distância <=200 else distância * 0.45 # modo simplificado   
 print('E o preço da sua passagem será de R${:.2f}'.format(preço))     
