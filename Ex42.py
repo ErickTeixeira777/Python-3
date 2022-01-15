df5597fb20f41a9133d268765fd63d60486fd252
Erick Teixeira <contatoerickvenon@gmail.com>
2022-01-15 07:31:39 -0300

    up date

---
+++ Ex42.py
@@ -0,0 +1,5 @@
+'''Refaça o Ex35 dos trângulos, acresentando o recurso de mostar que tipo de triângulo será formado:
+
+Equilátero: todos os lados iguais:
+Isósceles: dois lados iguais:
+Escaleno: todos os lados diferentes'''
