335ec011e2cec929887d241ff5332c2e57217bdd
Erick Teixeira <contatoerickvenon@gmail.com>
2022-01-15 13:32:44 -0300

    up date

--- Ex34.py
+++ Ex34.py
@@ -3,8 +3,8 @@ Para salário superiores a R$1.200,00\nCalcule um aumento de 10%.
 Para os inferiores ou iguais, o aumento é de 15%. '''
 
 salário = float(input('Qual é o salário do funcionário?'))
-if salário <= 1250:
+if salário <= 1300:
     novo = salário + (salário * 15/100)
 else:
     novo = salário + (salário * 10/100)
-print('Quem ganhava R${:.2f} passa a ganhar R${:.2f} agora.'.format(salário, novo))        
+print('Quem ganhava R${:.3f} passa a ganhar R${:.3f} agora.'.format(salário, novo))        
--- Ex36.py
+++ Ex36.py
@@ -1,3 +1,22 @@
 '''Escreva um programa para aprovar o empréstimo bancário para compra de uma casa. O programa vai perguntar o valor da casa, o salário do comprador e em quantos anos ele vai pagar.
 
 Calcule o valor da parcela mensal, sabendo que ela não pode exceder 30% do salário ou então o empréstimo será negado.'''
+print('===== Code-Save  Financiamentos ===== ')
+print('=' * 37)
+casa = float(input('Digite o valor da casa! '))
+print('=' * 37)
+salário = float(input('Salário do comprador: R$ '))
+print('=' * 37)
+anos = int(input('Quantos anos de financiamento? '))
+prestação = casa / (anos * 12)
+mínimo = salário * 30 / 100
+print('=' * 37)
+print('Para pagar uma casa de R${:.3f} \nEm {} anos!!! '.format(casa, anos))
+print('=' * 37)
+print('A prestação será de R${:.3f} '.format(prestação))
+print('=' * 37)
+if prestação <= mínimo:
+    print('Parabéns financiamento Aprovado!!!')
+else:
+    print('Sinto muito financiamento Negado!!!')
+print('=' * 37)    
