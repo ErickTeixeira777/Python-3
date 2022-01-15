16c308547778a6a8af12aa81a211d35b50fbf03e
Erick Teixeira <contatoerickvenon@gmail.com>
2022-01-15 07:42:50 -0300

    up date

---
+++ Ex44.py
@@ -0,0 +1,6 @@
+'''Elabore um programa que calcule o valor a ser pago por um produto considerando o seo preço normal e condição de pagamento:
+
+À vista dinheiro: 10% Off
+À vista débito: 5% Off
+Em 2x preço normal
+3x ou mais no crédito: 20% de juros'''
