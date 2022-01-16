2e3b7ad8c54efd62763f6cec604cd5b1c1644fca
Erick Teixeira <contatoerickvenon@gmail.com>
2022-01-16 13:40:29 -0300

    up date

--- Ex38.py
+++ Ex38.py
@@ -3,3 +3,13 @@
 -O 1° valor é maior
 -O 2° valor é maior
 -Não existe valor maior, os dois são iguais '''
+
+n1 = int(input('Primeiro número:'))
+n2 = int(input('Segundo número: '))
+if n1 > n2:
+    print('O Primeiro valor é maior:')
+elif n2 > n1:
+    print('O Segundo valor é maior:')   
+else:
+    print('Os dois valores são iguais!!!')     
+
