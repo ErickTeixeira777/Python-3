014907e7aaa319429b69e81310d0bf5e1bc3cac9
Erick Teixeira <contatoerickvenon@gmail.com>
2022-01-16 21:30:35 -0300

    up date

--- Ex40.py
+++ Ex40.py
@@ -8,3 +8,13 @@ RECUPERAÇÃO
 
 Média 7.0 ou superior:
 APROVADO'''
+n1 = float(input('Qual é sua 1° nota: '))
+n2 = float(input('Qual é sua 2° nota: '))
+m = (n1 + n2) / 2
+print('média {}'.format(m))
+if m < 5.0:
+    print('Reprovado! '.format(m))
+elif m == 5.0 or 6.9:
+    print('Recuperação! '.format(m))
+elif m > 7.0:
+    print('Aprovado! '.format(m))        
--- Ex41.py
+++ Ex41.py
@@ -5,3 +5,19 @@ Até 14 anos: INFANTIL
 Até 19 anos: JUNIOR
 Até 20 anos: SÊNIOR
 Acima: MASTER'''
+from datetime import date
+atual = date.today().year
+nasc = int(input('Digite o ano do Atleta: '))
+idade = atual - nasc 
+print('Você tem {} anos! '.format(idade))
+if idade <= 9:
+    print('Atleta MIRIM! ')
+elif idade <= 14:
+    print('Atleta INFANTIL! ')  
+elif idade <=19:
+    print('Atleta JUNIOR! ')
+else:
+    print('Atleta MASTER! ')    
+
+
+
