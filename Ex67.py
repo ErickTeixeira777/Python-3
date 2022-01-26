086c26529d3a5fd9c07755b0588f7decaf1916f3
Erick Teixeira <contatoerickvenon@gmail.com>
2022-01-26 04:35:50 -0300

    up date

--- Ex67.py
+++ Ex67.py
@@ -3,13 +3,18 @@ de vários números , um de cada vez
 para cada valor digitado pelo usuário
 o programa será interrompido quando o número
 solicitado for negativo'''
-print('{:=^40}'.format('\033[4;36;40mCode-Save\033[m'))
-print('{:=^40}'.format('\033[4;36;40mTabuada 2.0\033[m'))
-res = -1
-mult = num = 0
-while num * num:
-    num = int(input('Digite um número para ver sua tabuada: '))
-    mult * num
+print('{:=^51}'.format('\033[4;36;40mCode-Save\033[m'))
+print('{:=^51}'.format('\033[4;36;40mTabuada 2.0\033[m'))
+while True:
+    n = int(input('Digite um número para ver sua tabuada: '))
+    if n < 0:
+        break
+    print('='*40)
+    for c in range(1,11):
+        print(f'{n} x {c} = {n*c}')
+    print('='*40)  
+print('Programa Encerrado!') 
+print('='*40)        
     
     
   
