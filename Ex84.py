450138533660c6874bc5d8fd00bf2c5af86e5223
ErickTeixeira777 <contatoerickvenon@gmail.com>
2022-03-01 10:06:25 -0300

    up date

--- Ex84.py
+++ Ex84.py
@@ -3,3 +3,34 @@ No final mostre:
 A) Quantas pessoas foram cadastradas.
 B)Uma listagem com as pessoas mais pesadas.
 C)Uma listagem com as pessoas mais leves. '''
+temp = []
+princ = []
+nome = []
+mai = men = 0
+while True:
+    temp.append(str(input('Nome: ')))
+    temp.append(float(input('Peso: ')))
+    if len(princ) == 0:
+        mai = men = temp[1]
+    else:
+        if temp[1] > mai:
+            mai = temp[1]
+        if temp[1] < men:
+            men = temp[1]    
+    princ.append(temp[:])
+    temp.clear()
+    resp = str(input('Gostaria de continuar? [S/N] '))
+    if resp in 'Nn':
+        break
+print('='*30)       
+print(f'Ao todo, você cadastrou {len(princ)} pessoas! ')
+print(f'O maior peso foi de {mai} Kg. do(a): ', end='')
+for p in princ:
+    if p[1] == mai:
+        print(f'[{p[0]}] ', end='')
+print()        
+print(f'O menor peso foi de {men} kg. do(a): ', end='')
+for p in princ:
+    if p[1] == men:
+        print(f'[{p[0]}] ', end='') 
+print()        
